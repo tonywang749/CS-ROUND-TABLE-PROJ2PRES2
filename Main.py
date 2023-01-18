435f96253416489f1ce708cbb13ff6190fda4e88
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-01-18 09:35:20 -0500

    i think this does pig latin stuff

--- Main.py
+++ Main.py
@@ -1,2 +1,17 @@
+def get_index_first_vowel(input):
+    input = input.lower()
+    for char in input:
+        if char in "aeiou": return input.index(char)
+    return None
+
+def pig_latin(input):
+    if input[0] in "aeiou":
+        input = input + "ay"
+    else:
+        index = get_index_first_vowel(input)
+        input = input[index:] + input[0] + "ay"
+    return input
+
 def main():
-    print("Hello World")
+    text = input("Enter a word: ")
+    print(f'In pig latin, your text would be {pig_latin(text)}')
